f05e96c29e16cd193beb938c2ac0f4e7a5e30c11
mkoroleva5 <mkoroleva5@gmail.com>
2023-02-08 22:01:55 +0300

    feat: add devtools helper  from zustand

--- src/store/roomStore.ts
+++ src/store/roomStore.ts
@@ -1,25 +1,28 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import { RoomStoreTypes } from './roomStoreTypes';
 
-export const useRoomState = create<RoomStoreTypes>((set) => ({
-  players: [],
-  roomId: '',
-  userId: null,
-  status: null,
+export const useRoomState = create(
+  devtools<RoomStoreTypes>((set) => ({
+    players: [],
+    roomId: '',
+    userId: null,
+    status: null,
 
-  updateUserId: (id: string) => {
-    set((state: RoomStoreTypes) => {
-      return { ...state, userId: id };
-    });
-  },
+    updateUserId: (id: string) => {
+      set((state: RoomStoreTypes) => {
+        return { ...state, userId: id };
+      });
+    },
 
-  /* addPlayers: (newPlayer: IPlayerResponse) =>
+    /* addPlayers: (newPlayer: IPlayerResponse) =>
     set((state: RoomStoreTypes) => {
       return { ...state, players: [...state.players, newPlayer] };
     }), */
 
-  changeStatus: (status: string) =>
-    set((state: RoomStoreTypes) => {
-      return { ...state, status };
-    }),
-}));
+    changeStatus: (status: string) =>
+      set((state: RoomStoreTypes) => {
+        return { ...state, status };
+      }),
+  })),
+);
