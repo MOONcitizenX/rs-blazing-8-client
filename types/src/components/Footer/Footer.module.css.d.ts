9e4ebfa437280b99d4b10ec77fbc8b39573f9d8f
mkoroleva5 <mkoroleva5@gmail.com>
2023-02-25 20:41:52 +0300

    feat: add footer to StartPage

---
+++ types/src/components/Footer/Footer.module.css.d.ts
@@ -0,0 +1,18 @@
+declare const styles: {
+  readonly "footerWrapper": string;
+  readonly "footer": string;
+  readonly "githubWrapper": string;
+  readonly "githubMenu": string;
+  readonly "githubMenuImage": string;
+  readonly "githubLinksWrapper": string;
+  readonly "active": string;
+  readonly "githubLink": string;
+  readonly "githubLogo": string;
+  readonly "githubName": string;
+  readonly "year": string;
+  readonly "rsWrapper": string;
+  readonly "rsLink": string;
+  readonly "rsLogo": string;
+};
+export = styles;
+
